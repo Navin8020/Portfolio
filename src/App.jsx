7954f7ebb48b8fb7c47c78109c603579ecffd812
Navin <knaveeen34@gmail.com>
2026-09-18 16:30:38 +0530

    Fix component import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
